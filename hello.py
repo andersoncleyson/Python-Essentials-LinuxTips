1de27b8a3251131bedcc2f5c3738400434fbfb41
Anderson <andersoncleyson@protonmail.com>
2023-10-01 16:12:45 -0300

    Input & Output e args CLI

--- hello.py
+++ hello.py
@@ -19,20 +19,44 @@ Execução:
     ./hello.py
 """
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __author__ = "Anderson Cleyson"
 __license__ = "Unlicense"
 
 import os
+import sys
 
-current_language = os.getenv("LANG")[:5]
+arguments = {
+    "lang": None,
+    "count": 1
+}
+
+for arg in sys.argv[1:]:
+    # TODO: Tratar ValueError
+    key, value = arg.split("=")
+    key = key.lstrip("-").strip()
+    value = value.strip()
+    if key not in arguments:
+        print(f"Invalid Option `{key}`")
+        sys.exit()
+    arguments[key] = value
+
+current_language = arguments["lang"]
+if current_language is None:
+    # TODO: Usar repetição
+    if "LANG" in os.environ:
+        current_language = os.getenv("LANG")
+    else:
+        current_language = input("Choose a language:")
+
+current_language = current_language[:5]
 
 msg = {
     "en_US": "Hello, friend",
     "pt_BR": "Olá, amigo",
-    "es_SP": "Hola, amigo",
+    "es_ES": "Hola, amigo",
     "fr_FR": "Bonjour monde",
     "it_IT": "Ciao, mondo"
 }
 
-print(msg[current_language])
+print(msg[current_language] * int(arguments["count"]))
