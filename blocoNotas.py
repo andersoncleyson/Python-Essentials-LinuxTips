cfaae6111c5ca34c67cc062baec05e9db4d0ed1a
Anderson <andersoncleyson@protonmail.com>
2023-10-06 17:10:41 -0300

    Bloco de Notas

--- blocoNotas.py
+++ blocoNotas.py
@@ -15,19 +15,35 @@ __version__ = "0.1.0"
 import os
 import sys
 
+cmds = ("read", "new")
 path = os.curdir
 filepath = os.path.join(path, "notes.txt")
 
 arguments = sys.argv[1:]
 if not arguments:
     print("Invalid usage")
+    print(f"You must specify subcommand {cmds}")
     sys.exit(1)
 
-cmds = ("read", "new")
 if arguments[0] not in cmds:
     print(f"Invalid command {arguments[0]}")
 
 if arguments[0] == "read":
-    #leitura
+    # leitura das notas
+    for line in open(filepath):
+        title, tag, text = line.split("\t")
+        if tag.lower() == arguments[1].lower():
+            print(f"titulo: {title}")
+            print(f"text: {text}")
+            print("-" * 30)
+            print()
 if arguments[0] == "new":
-    #criação da nota
+    titulo = arguments[1] # TODO: Tratar exception
+    text = [
+        f"{titulo}\n",
+        input("tag:").strip(),
+        input("text:\n").strip(),
+    ]
+    # \t - tsv
+    with open(filepath, "a") as file_:
+        file_.write("\t".join(text) + "\n")
