24b7e051b2046bc99df0a61cf767c2f9ba157d29
unknown <andersoncleyson@protonmail.com>
2023-10-15 18:21:53 -0300

    List and Dict comprehension

--- repete.py
+++ repete.py
@@ -1,10 +1,34 @@
 #!/usr/bin/env python3
 
-# numbers = [1, 2, 3, 4, 5, 6]
-numbers = range(1, 11) # start, next, stop
-# Iterable
-for number in numbers:
-    if number % 2 == 0:
-        print(number)
-    else:
-        continue
+# For loops / Laço for
+"""
+dados = {}
+for line in open("post.txt"):
+    if line == "----\n":
+        break
+    key, valor = line.split(":")
+    dados[key] = valor.strip()
+
+print(dados)
+"""
+#Estrutural
+"""
+nova = []
+for n in [1, 2, 3]:
+    nova.append(n * 2)
+print(nova)
+"""
+
+original = [1, 2, 3]
+# Funcional
+# List comprehension
+dobrada = [n * 2 for n in original]
+print(dobrada)
+
+# Dict comprehension
+dados = {
+    line.split(":")[0]: line.split(":")[1]
+    for line in open("post.txt")
+    if ":" in line
+}
+print(dados)
