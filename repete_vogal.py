d6558c87320bce83e319ad462d0d18a27d2fd4db
Anderson Cleyson Soares e Silva <andersoncleyson@protonmail.com>
2023-10-17 18:06:32 -0300

    repete vogal

--- repete_vogal.py
+++ repete_vogal.py
@@ -3,7 +3,7 @@
 words = []
 while True:
     word = input("Digite uma palavra (ou enter para sair):").strip()
-    if not word:
+    if not word: # condição de parada
         break
     final_word = ""
     for letter in word:
@@ -12,6 +12,11 @@ while True:
             final_word += letter * 2
         else:
             final_word += letter
+        
+        final_word += letter
+
+        # If ternário alternativo
+        #final_word += letter * 2 if letter.lower() in "aeiou" else letter
     words.append(final_word)
 
 print(*words, sep="\n")
