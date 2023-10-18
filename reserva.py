3d99daf43eb9959da15fde1db2e1c3852190718e
Anderson Cleyson Soares e Silva <andersoncleyson@protonmail.com>
2023-10-18 18:10:39 -0300

    Exercicios pt3 - reserva

--- reserva.py
+++ reserva.py
@@ -3,7 +3,14 @@ import logging
 
 quartos = {}
 try:
-    for line in open("")
+    for line in open("quartos.txt"):
+        codigo, nome, preco = line.strip().split(",")
+        quartos[codigo] = {
+            "nome": nome,
+            "preco": float(preco), #TODO: Decimal
+        }
 except FileNotFoundError:
     logging.error("Arquivo não existe")
     sys.exit(1)
+
+print(quartos)
